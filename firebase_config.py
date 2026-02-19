8fe9d125b94743d21bbe4c1a4c875f4f65fb5149
9998884622 <priya1722006@gmail.com>
2026-02-19 10:12:08 +0530

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -1,20 +1,26 @@
+import streamlit as st
 import firebase_admin
 from firebase_admin import credentials, storage, db
-from datetime import datetime
 
-cred = credentials.Certificate("firebase_service_account.json")  # Replace with your service account
+cred_dict = {
+    "type": st.secrets["FIREBASE_TYPE"],
+    "project_id": st.secrets["FIREBASE_PROJECT_ID"],
+    "private_key_id": st.secrets["FIREBASE_PRIVATE_KEY_ID"],
+    "private_key": st.secrets["FIREBASE_PRIVATE_KEY"].replace("\\n", "\n"),
+    "client_email": st.secrets["FIREBASE_CLIENT_EMAIL"],
+    "client_id": st.secrets["FIREBASE_CLIENT_ID"],
+    "auth_uri": st.secrets["FIREBASE_AUTH_URI"],
+    "token_uri": st.secrets["FIREBASE_TOKEN_URI"],
+    "auth_provider_x509_cert_url": st.secrets["FIREBASE_AUTH_PROVIDER_X509_CERT_URL"],
+    "client_x509_cert_url": st.secrets["FIREBASE_CLIENT_X509_CERT_URL"]
+}
+
+cred = credentials.Certificate(cred_dict)
+
 firebase_admin.initialize_app(cred, {
-    'storageBucket': 'your-project-id.appspot.com',   # Replace
-    'databaseURL': 'https://your-project-id.firebaseio.com/'  # Replace
+    'storageBucket': f'{st.secrets["FIREBASE_PROJECT_ID"]}.appspot.com',
+    'databaseURL': f'https://{st.secrets["FIREBASE_PROJECT_ID"]}.firebaseio.com/'
 })
 
 bucket = storage.bucket()
 ref = db.reference('intruder_data')
-
-def upload_intruder_image(file_path, filename):
-    blob = bucket.blob(f"intruder/{filename}")
-    blob.upload_from_filename(file_path)
-    return blob.public_url
-
-def save_location(user_id, lat, lng):
-    ref.child(user_id).set({'lat': lat, 'lng': lng})
