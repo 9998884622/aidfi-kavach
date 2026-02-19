58a7ed2b23e92a83ab4bdf188e050e7442c2a342
9998884622 <priya1722006@gmail.com>
2026-02-19 12:39:16 +0530

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -1,18 +1,64 @@
-files=[]
-locations={}
+import json
+import os
+
+FILES="files_db.json"
+LOC="location_db.json"
+
+# create if not exist
+
+if not os.path.exists(FILES):
+
+    with open(FILES,"w") as f:
+
+        json.dump([],f)
+
+if not os.path.exists(LOC):
+
+    with open(LOC,"w") as f:
+
+        json.dump({},f)
+
 
 def upload_file(path,name):
 
-    files.append(name)
+    with open(FILES) as f:
+
+        data=json.load(f)
+
+    data.append(name)
+
+    with open(FILES,"w") as f:
+
+        json.dump(data,f)
+
 
 def get_files():
 
-    return files
+    with open(FILES) as f:
+
+        return json.load(f)
+
 
 def save_location(user,lat,lng):
 
-    locations[user]=[lat,lng]
+    with open(LOC) as f:
+
+        data=json.load(f)
+
+    data[user]={
+
+        "Latitude":lat,
+        "Longitude":lng
+
+    }
+
+    with open(LOC,"w") as f:
+
+        json.dump(data,f)
+
 
 def get_locations():
 
-    return locations
+    with open(LOC) as f:
+
+        return json.load(f)
